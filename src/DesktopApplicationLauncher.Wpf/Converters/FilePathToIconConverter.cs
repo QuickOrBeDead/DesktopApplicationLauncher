c63390590405495f3c7c00a224465cf036352991
BoraA <boraa@d-teknoloji.com.tr>
2021-04-03 15:17:56 +0300

    Set app icon to folder icon when filePath is null or empty

--- src/DesktopApplicationLauncher.Wpf/Converters/FilePathToIconConverter.cs
+++ src/DesktopApplicationLauncher.Wpf/Converters/FilePathToIconConverter.cs
@@ -5,6 +5,7 @@
     using System.Drawing;
     using System.Globalization;
     using System.IO;
+    using System.Runtime.InteropServices;
     using System.Windows;
     using System.Windows.Data;
     using System.Windows.Interop;
@@ -30,20 +31,68 @@
         {
             var defaultIcon = SystemIcons.WinLogo;
             var iconForFile = defaultIcon;
-            if (filePath != null && File.Exists(filePath))
+            try
             {
-                try
+                if (string.IsNullOrWhiteSpace(filePath))
+                {
+                    iconForFile = DefaultIcons.FolderLarge;
+                }
+                else if (File.Exists(filePath))
                 {
                     iconForFile = Icon.ExtractAssociatedIcon(filePath) ?? defaultIcon;
                 }
-                catch
-                {
-                    // Empty
-                    iconForFile = defaultIcon;
-                }
+            }
+            catch
+            {
+                // Empty
+                iconForFile = defaultIcon;
             }
 
             return iconForFile;
+        }
+
+        private static class DefaultIcons
+        {
+            private static readonly Lazy<Icon> _lazyFolderIcon = new(FetchIcon, true);
+
+            public static Icon FolderLarge => _lazyFolderIcon.Value;
+
+            private static Icon FetchIcon()
+            {
+                var tmpDir = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString())).FullName;
+                var icon = ExtractFromPath(tmpDir);
+                Directory.Delete(tmpDir);
+                return icon;
+            }
+
+            private static Icon ExtractFromPath(string path)
+            {
+                var shinfo = new SHFILEINFO();
+                SHGetFileInfo(
+                    path,
+                    0, ref shinfo, (uint)Marshal.SizeOf(shinfo),
+                    SHGFI_ICON);
+                return Icon.FromHandle(shinfo.hIcon);
+            }
+
+            //Struct used by SHGetFileInfo function
+            [StructLayout(LayoutKind.Sequential)]
+            private struct SHFILEINFO
+            {
+                public IntPtr hIcon;
+                public int iIcon;
+                public uint dwAttributes;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+                public string szDisplayName;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+                public string szTypeName;
+            };
+
+            [DllImport(dllName: "shell32.dll", CharSet = CharSet.Unicode)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+            private static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+
+            private const uint SHGFI_ICON = 0x100;
         }
     }
 }
